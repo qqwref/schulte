eeec8c08735fb411039446966aa1d3d52cd43db2
James Sarginson <68438886+Jaybrainer@users.noreply.github.com>
2022-05-14 12:27:13 -0700

    blindfold debug

--- js/schulte.js
+++ js/schulte.js
@@ -565,6 +565,7 @@ vueApp = new Vue({
                     }
                     if (this.blindMode) {
                         if (true /*this.stats.correctClicks == 1*/) {
+                            alert(this.stats.correctClicks);
                             for (let i = 0; i < this.cells.length; i++) {
                                 this.cells[i].colorStyle = "color: transparent";
                             }
